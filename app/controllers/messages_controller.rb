2b21c7a70b773e3bf962f733e5faa25a76ecaae1
Colin Newman <colinnewman100@gmail.com>
2023-07-24 00:38:23 -0700

    Working on conversations#show view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,10 @@
 class MessagesController < ApplicationController
   before_action :set_conversation
   def create
-    @message = @conversation.messages.build(conversation_params)
+    @message = @conversation.messages.build(message_params)
     @message.user = @current_user
 
-    
-      
-
+    @message.save
   end
 
   def delete
@@ -14,7 +12,7 @@ class MessagesController < ApplicationController
 
   private
     def set_conversation
-      @conversation = Conversation.visible_to_user.find(params[:conversation_id])
+      @conversation = Conversation.visible_to_user(@current_user).find(params[:conversation_id])
     end 
 
     def message_params
