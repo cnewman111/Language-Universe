21f8ccfce80e87f7f0b7d33429f786afe3092b66
Colin Newman <colinnewman100@gmail.com>
2023-07-25 13:28:31 -0700

    just installed devise, fixing conflicts with existing user system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  devise_for :users
   resources :prompts do 
     resources :conversations, except: [:update, :edit, :index] do
       resources :messages, only: [:create, :delete]
