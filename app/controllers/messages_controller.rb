23d3428b8114347ebcfcca2cd9187172764f6bd0
Colin Newman <colinnewman100@gmail.com>
2023-09-04 14:26:56 -0700

    Clear message on submit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,17 @@
 class MessagesController < ApplicationController
   before_action :set_conversation
+  before_action :set_prompt
+
   def create
     @message = @conversation.messages.build(message_params)
     @message.creating_entity = "user"
     
     is_saved = @message.save
-    render turbo_stream: turbo_stream.replace('message_errors', partial: 'message_errors', locals: { message: @message })
+    render turbo_stream: [
+      turbo_stream.replace('message_form', partial: "messages/message_form", locals: { message: @new_message }),
+      turbo_stream.replace('message_errors', partial: "message_errors", locals: { message: @message })
+    ]
+
     if is_saved
       # Prompt conversation model for response?
       @conversation.create_responses
@@ -15,6 +21,10 @@ class MessagesController < ApplicationController
   private
     def set_conversation
       @conversation = Conversation.visible_by_user(current_or_guest_user).find(params[:conversation_id])
+    end 
+
+    def set_prompt
+      @prompt = Prompt.visible_by_user(current_or_guest_user).find(params[:prompt_id])
     end 
 
     def message_params
